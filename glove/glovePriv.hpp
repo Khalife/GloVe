ecdc490c0ee779b17d80c1f0e8de59dc79e77f52
Sammy Khalife <khalife.sammy@gmail.com>
2017-01-24 16:06:15 +0100

    Initial Commit

---
+++ glove/glovePriv.hpp
@@ -0,0 +1,19 @@
+/*
+ *  glovePriv.hpp
+ *  glove
+ *
+ *  Created by Sammy on 24/01/2017.
+ *
+ *
+ */
+
+/* The classes below are not exported */
+#pragma GCC visibility push(hidden)
+
+class glovePriv
+{
+    public:
+    void HelloWorldPriv(const char *);
+};
+
+#pragma GCC visibility pop
